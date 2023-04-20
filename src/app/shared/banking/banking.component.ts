e2ba187393a741a98e4ca9f32d2add06278c7fc0
Suelen <suelen_santos2002@outlook.com>
2023-04-20 11:14:25 -0300

    test content created

--- src/app/shared/banking/banking.component.ts
+++ src/app/shared/banking/banking.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./banking.component.css']
 })
 export class BankingComponent {
+  private savings: number = 300;
+  private account: number = 200;
 
+  get getSavings(): number{
+    return this.savings;
+  }
+
+  get getAccount(): number{
+    return this.account;
+  }
+
+  public setWithdraw(value:string): number | undefined{
+    const withdraw = Number(value);
+
+    if(isNaN(withdraw) || this.savings < withdraw){
+      return;
+    }
+
+    this.savings -= withdraw;
+    return (this.account += withdraw);
+  }
+
+  public setDeposit(value:string): number | undefined{
+    const deposit = Number(value);
+
+    if(isNaN(deposit) || this.account < deposit){
+      return;
+    }
+
+    this.account -= deposit;
+    return (this.savings += deposit);
+  }
 }
