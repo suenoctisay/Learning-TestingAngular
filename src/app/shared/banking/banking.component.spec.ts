a8c9829cfea8e3caedf364ee7617ee1ff32c198c
Suelen <suelen_santos2002@outlook.com>
2023-04-24 07:40:41 -0300

    banking component test

--- src/app/shared/banking/banking.component.spec.ts
+++ src/app/shared/banking/banking.component.spec.ts
@@ -20,4 +20,64 @@ describe('BankingComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('(U) getSavings(): should have initial value of 300', () => {
+    expect(component.getSavings).toEqual(300);
+  });
+
+  it('(U) getAccount(): should have initial value of 200', () => {
+    expect(component.getAccount).toEqual(200);
+  });
+
+  it('(U) setWithdraw(): should transfer value from savings to account', () => {
+    component.setWithdraw('300');
+    expect(component.getSavings).toEqual(0);
+    expect(component.getAccount).toEqual(500);
+  });
+
+  it('(U) setWithdraw(): should not transfer value from savings to account if = string || value > savings amount', () => {
+    expect(component.setWithdraw('string')).not.toBeTruthy();
+    expect(component.setWithdraw('1000')).not.toBeTruthy();
+
+    expect(component.getSavings).toEqual(300);
+    expect(component.getAccount).toEqual(200);
+  });
+
+  it('(U) setDeposit(): should transfer value from account to savings', () => {
+    component.setDeposit('200');
+    expect(component.getAccount).toEqual(0);
+    expect(component.getSavings).toEqual(500);
+  });
+
+  it('(U) setDeposit(): should not transfer value from account to savings if = string || value > account amount', () => {
+    expect(component.setDeposit('string')).not.toBeTruthy();
+    expect(component.setDeposit('1000')).not.toBeTruthy();
+
+    expect(component.getSavings).toEqual(300);
+    expect(component.getAccount).toEqual(200);
+  });
+
+  // ========================================================================================================================= //
+
+  it ('(I) setDeposit(): should transfer amount from account to savings', () => {
+    let element = fixture.debugElement.nativeElement;
+
+    element.querySelector('#input-deposit').value = '50';
+    element.querySelector('#button-deposit').click();
+    fixture.detectChanges();
+
+    expect(element.querySelector('#get-savings').textContent).toEqual('350');
+    expect(component.getAccount).toEqual(150);
+  });
+
+  it ('(I) setWithdraw(): should transfer amount from savings to account', () => {
+    let element = fixture.debugElement.nativeElement;
+
+    element.querySelector('#input-withdraw').value = '50';
+    element.querySelector('#button-withdraw').click();
+    fixture.detectChanges();
+
+    expect(element.querySelector('#get-account').textContent).toEqual('250');
+    expect(component.getSavings).toEqual(250);
+  });
 });
