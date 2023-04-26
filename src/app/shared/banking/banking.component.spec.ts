cd979f5987e58ff1186c9d0a7b903e34d40b8a02
Suelen <suelen_santos2002@outlook.com>
2023-04-26 07:53:13 -0300

    investments imports

--- src/app/shared/banking/banking.component.spec.ts
+++ src/app/shared/banking/banking.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BankingComponent } from './banking.component';
+import { InvestmentsComponent } from '../investments/components/lists/investments.component';
 
 describe('BankingComponent', () => {
   let component: BankingComponent;
@@ -8,7 +9,7 @@ describe('BankingComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BankingComponent ]
+      declarations: [ BankingComponent , InvestmentsComponent]
     })
     .compileComponents();
 
